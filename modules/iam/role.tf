6b42c8134bbdd1e1dafe0db60cf08df3f3106dfe
PakeezaPakeeza <zunairapakeeza3@gmail.com>
2025-10-23 19:18:41 +0200

    Update role.tf

--- modules/iam/role.tf
+++ modules/iam/role.tf
@@ -1,4 +1,9 @@
 #That means this role is meant to be attached to EC2 instances — not users — so those EC2s can access AWS APIs (like S3, CloudWatch, etc.) without hardcoding access keys.
+#ws_iam_role is just a stand-alone role (a “template”). It isn’t used by any EC2 until you:
+	#1.	Give it permissions (attach policies),
+	#2.	Wrap it in an instance profile, and
+	#3.	Reference that instance profile in your EC2 resources.
+
 
 resource "aws_iam_role" "ec2_role" {
   name = "${var.rolename}"
